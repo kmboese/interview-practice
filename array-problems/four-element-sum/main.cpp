080f3022ebe3825b502f398226aa9a4e28219d34
Kevin Boese <kmboese@ucdavis.edu>
2018-05-04 20:24:33 -0700

    started recursive sum function

--- array-problems/four-element-sum/main.cpp
+++ array-problems/four-element-sum/main.cpp
@@ -28,7 +28,8 @@ uniform_int_distribution<int>  dist(0, UPPER_BOUND);
 
 /* Algorithms */
 int fourElementSum(int *input, int n, int size);
-int FESRecursive(int leftBound, int rightBound, int subtotal, int level);
+int FESRecursive(int *input, int bound, int solutions, int key, int index, int leftBound, int rightBound, int subtotal, int level);
+void printArray(int *a, int bound, int index);
 
 /* Test Functions */
 void testSingle();
@@ -36,12 +37,23 @@ void testMultiple();
 void testLarge();
 void testRecursive();
 
+
 int main(int argc, char **argv) {
 	
 
 	testSingle();
 	testMultiple();
 	testLarge();
+	testRecursive();
+
+
+	/*
+	int size = 5;
+	int a[size] {1, 2, 3, 4, 5};
+	printArray(a, size-1, 0);
+	*/
+	
+
 	
 	return 0;
 }
@@ -66,7 +78,7 @@ void testMultiple() {
 }
 
 void testLarge() {
-	int size = 600;
+	int size = 100;
 	//The average value is approx. (UPPER_BOUND / 2), so 4 of those
 	// added together should be a commonly found sum for four-element-sum.
 	int value = (UPPER_BOUND / 2) / 4;
@@ -80,7 +92,22 @@ void testLarge() {
 }
 
 void testRecursive() {
-	
+	cout << "\nTesting 4-El-Sum with recursive function...\n" << divider;
+	int size = 6;
+	int input[6] {1, 0, 5, 2, 0, 3};
+	int n = 8;
+	int solutions = FESRecursive(input, size, 0, n, 0, 0, size-ELEMENTS, 0, 1);
+	cout << "Found " << solutions << " solutions\n"; 
+}
+
+void printArray(int *a, int bound, int index) {
+	//Base case
+	if (index > bound){
+		cout << endl;
+		return;
+	}
+	cout << a[index] << " ";
+	printArray(a, bound, ++index);
 }
 
 int fourElementSum(int *input, int n, int size) {
@@ -119,6 +146,19 @@ int fourElementSum(int *input, int n, int size) {
 	return solutions;
 }
 
-int FESRecursive(int leftBound, int rightBound, int subtotal, int level) {
-
+int FESRecursive(int *input, int bound, int solutions, int key, int index, int leftBound, int rightBound, int subtotal, int level) {
+	if (index == bound)
+		return solutions;
+	/*
+	//Base case: at level four
+	if (level == ELEMENTS) {
+		if (subtotal + input[index] == key)
+			solutions++;
+		else if (index == (size-1))
+			return;
+		else
+			FESRecursive(input, size, solutions, key, ++index, leftBound, rightBound, subtotal, level);
+	}
+	*/
+	FESRecursive(input, bound, solutions, key, ++index, leftBound, rightBound, subtotal, ++level);
 }
